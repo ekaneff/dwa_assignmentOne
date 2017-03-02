ec015d4ef9183d1204c2255efdc5ce607df1613a
Emily Kaneff <emilykaneff@Emilys-MacBook-Pro.local>
2017-03-02 15:15:53 -0500

    testing unit test fail case

--- test/test.js
+++ test/test.js
@@ -2,7 +2,11 @@ var assert = require('assert');
 describe('Array', function() {
   describe('#indexOf()', function() {
     it('should return -1 when the value is not present', function() {
-      assert.equal(-1, -1);
+      if (1==-1) {
+      	done();
+      } else {
+      	console.log("Unit test failed. Commit aborted");
+      }
     });
   });
 });
