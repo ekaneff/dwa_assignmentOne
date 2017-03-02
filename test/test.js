1e28c2024e09e777e82bb55a37e7d57909c13243
Emily Kaneff <emilykaneff@Emilys-MacBook-Pro.local>
2017-03-02 18:46:50 -0500

    still trying to fix post merge

--- test/test.js
+++ test/test.js
@@ -3,7 +3,7 @@ describe('Array', function() {
   describe('#indexOf()', function() {
     it('Running unit test...', function(done) {
       if (1==1) {
-      	console.log("Test passed!");
+      	console.log("test passed!");
       	done();
       } else {
       	console.log("Unit test failed. Commit aborted.");
