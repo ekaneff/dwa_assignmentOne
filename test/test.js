3f5213b0cc9d3963b4dc92c2f643166855bf5e78
Emily Kaneff <emilykaneff@Emilys-MacBook-Pro.local>
2017-03-02 19:20:40 -0500

    again with post merge

--- test/test.js
+++ test/test.js
@@ -1,7 +1,7 @@
 var assert = require('assert');
 describe('Array', function() {
   describe('#indexOf()', function() {
-    it('Running unit test...', function(done) {
+    it('Running unit test..', function(done) {
       if (1==1) {
       	console.log("test passed!");
       	done();
